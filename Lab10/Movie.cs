8d707cef1acf2aed01e3a17318293970a0ae3dfc
Aaron <devilfruits1999@gmail.com>
2019-10-24 16:37:43 -0400

    First Commit

---
+++ Lab10/Movie.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10
+{
+    class Movie
+    {
+        private string movieTitle;
+        private string movieGenre;
+
+        public string MovieTitle { get { return movieTitle; } set { movieTitle = value; } }
+        public string MovieGenre { get { return movieGenre; } set { movieGenre = value; } }
+
+        public Movie(string _movieTitle, string _movieGenre) 
+        {
+            movieTitle = _movieTitle;
+            movieGenre = _movieGenre;
+        }
+        public override string ToString()
+        {
+            return $"Movie Title: {MovieTitle}, Movie Genre: {MovieGenre}";
+        }
+        public static void PrintMoviesByGenre(List<Movie> movies, string genre) 
+        {
+            foreach (Movie item in movies)
+            {
+                if (item.MovieGenre == genre) 
+                {
+                    Console.WriteLine(item.ToString());
+                } 
+            }
+        }
+        public static List<string> GetMovieGenres(List<Movie> movies)
+        {
+            List<string> movieGenres = new List<string>();
+            foreach (Movie movie in movies)
+            {
+                if (!movieGenres.Contains(movie.MovieGenre))
+                {
+                    movieGenres.Add(movie.MovieGenre);
+                }
+            }
+            return movieGenres;
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lab10
+{
+    class Program
+    {
+        static string exiting = "n";
+        static void Main(string[] args)
+        {
+
+            List<Movie> Movies = new List<Movie>()
+            {
+                new Movie("Harry Potter", "Fantasy"),
+                new Movie("Lord of Rings", "Fantasy"),
+                new Movie("Iron Man", "SuperHero"),
+                new Movie("Spider Man", "SuperHero"),
+                new Movie("John Wick", "Action"),
+                new Movie("The Matrix", "Action"),
+                new Movie("The Land Before Time", "Animation"),
+                new Movie("The Lion King", "Animation"),
+                new Movie("Us", "Horror"),
+                new Movie("It", "Horror"),
+                new Movie("Step Brothers", "Comedy"),
+                new Movie("Anchorman", "Comedy")
+            };
+            
+            while (exiting != "y")
+            {
+                PrompUser(Movies);
+                Rerun();
+            }
+
+        }
+        static void Rerun()
+        {
+            exiting = GetInput("Would you like to exit?(y/n): ");
+        }
+        static void PrompUser(List<Movie> movies) 
+        {
+            List<string> movieGenres = Movie.GetMovieGenres(movies);
+            string genreMessage = "";
+            for (int i = 0; i < movieGenres.Count; i++)
+            {
+                genreMessage = string.Concat(genreMessage, $"{i+1}) {movieGenres[i]}\n");
+            }
+            Console.WriteLine("Select A Genre:");
+            Movie.PrintMoviesByGenre(movies, movieGenres[ParseIntFromString(genreMessage)]);
+        }
+        static string GetInput(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            if (input == "")
+            {
+            Console.WriteLine("Input cannot be blank");
+                return GetInput(message);
+            }
+            else
+            {
+                return input;
+            }
+        }
+        static int ParseIntFromString(string message)
+        {
+            try
+            {
+                return int.Parse(GetInput(message))-1;
+            }
+            catch
+            {
+                Console.WriteLine("Something went wrong, please try again: ");
+                return ParseIntFromString(message);
+            }
+        }
+    }
+}
+
